c9235bb3fe2f5c9f721715428faac027e5f027b3
John <john791016@gmail.com>
2022-06-29 15:43:47 +0800

    change database location

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST':os.environ.get('DB_HOST'),
+        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
@@ -129,4 +129,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL='core.User'
+AUTH_USER_MODEL = 'core.User'
