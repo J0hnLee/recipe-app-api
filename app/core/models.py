8ff2592d4d0efe7b6ed8fc4cd30b2d3dc2db9dc8
Lee Cheng Hsun <john791016@gmail.com>
2022-07-11 21:07:55 +0800

    add image api

--- app/core/models.py
+++ app/core/models.py
@@ -2,13 +2,23 @@
 Database models
 
 """
+import uuid
+import os
 
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin)
+    PermissionsMixin
+)
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -57,7 +67,7 @@ class Recipe(models.Model):
     ingredients = models.ManyToManyField('Ingredient')
     # ingredients=models.TextField(blank=True)
     # recipe=models.TextField(blank=True)
-    # image=models.ImageField(null=True,upload_to='recipes/')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
