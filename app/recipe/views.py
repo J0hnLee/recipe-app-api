78772e362a6594d96b7d03a4bf88465d51e48058
Lee Cheng Hsun <john791016@gmail.com>
2022-07-14 21:10:22 +0800

    add filter func

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,6 +2,14 @@
 Views for the recipe APIs.
 
 """
+# from drf_spectacular.utils import (
+#     extend_schema_views,
+#     extend_schema,
+#     OpenApiParameter,
+#     OpenApiTypes,
+# )
+
+
 from rest_framework import (
     viewsets,
     mixins,
@@ -26,15 +34,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve recipes for the current authenticated user only."""
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset.filter(ingredients__isnull=False)
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
 
-        return self.queryset.filter(
-            user=self.request.user).order_by('-id')
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-id').distinct()
 
-    # def perform_create(self, serializer):
-    #     """Create a new recipe."""
-    #     serializer.save(user=self.request.user)
+        # return self.queryset.filter(
+        #     user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
